c122c09f8be48aaf8d40a776d317a15a87507faa
Md Wasin Ahmed <wasinahmed87@gmail.com>
2025-12-14 12:34:34 +0600

    Second commit

--- Menu.sh
+++ Menu.sh
@@ -1,59 +1,53 @@
 #!/bin/bash
-
-prime_check() {
+ 
+check_prime() {
     n=$1
     if [ $n -le 1 ]; then
-        echo "not prime"
+        echo "$n is not prime."
         return
     fi
-    for ((i=2; i*i<=n; i++))
-    do
-        if [ $((n%i)) -eq 0 ]; then
-            echo "$n not prime"
-            return
+    for ((i=2; i*i<=n; i++)); do
+      if [ $((n % i)) -eq 0 ]; then
+       echo "$n is not prime."
+          return
         fi
     done
-    echo "prime"
+    echo "$n is prime."
 }
 
-leap_check() {
-    y=$1
-    if [ $((y%400)) -eq 0 ] || ([ $((y%4)) -eq 0 ] && [ $((y%100)) -ne 0 ]); then
-        echo "$y is leap year"
+check_leap() {
+    year=$1
+    if (( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )); then
+        echo "$year is a leap year."
     else
-        echo "$y not leap year"
+        echo "$year is not a leap year."
     fi
 }
 
 sum_series() {
     n=$1
     sum=0
-    for ((i=1; i<=n; i++))
-    do
-        sum=$((sum+i))
+    for ((i=1; i<=n; i++)); do
+        sum=$((sum + i))
     done
-    echo "sum = $sum"
+    echo "Sum of series = $sum"
 }
 
 read -p "Enter a number: " num
 
-while true
-do
-    echo "1. check prime"
-    echo "2. check leap year"
-    echo "3. sum of series"
-    echo "4. exit"
-    read -p "choose option: " ch
-    
-  
-    ch=$(echo $ch | tr -d '[:space:]')
-    
-    case $ch in
-        1) prime_check $num ;;
-        2) leap_check $num ;;
+while true; do
+    echo "Menu:"
+    echo "1) Check Prime"
+    echo "2) Check Leap Year"
+    echo "3) Print Sum of Series"
+    echo "4) Exit"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) check_prime $num ;;
+        2) check_leap $num ;;
         3) sum_series $num ;;
         4) echo "Exiting..."; exit 0 ;;
-        *) echo "Invalid choice" ;;
+        *) echo "Invalid choice, try again." ;;
     esac
-    echo ""
 done
